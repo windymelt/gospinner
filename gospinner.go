364950bf8a75977ed38871bbdd5d4a3056057332
windymelt <windymelt@3qe.us>
2017-09-15 22:23:01 +0900

    Did gofmt

--- gospinner.go
+++ gospinner.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 )
 
-func main () {
-     chs := [4]rune{ '|', '/', '-', '\\' }
-     i := 0
-     for {
-       fmt.Printf("\r%c", chs[i])
-       i = (i + 1) % 4
-       time.Sleep(100 * time.Millisecond)
-     }
+func main() {
+	chs := [4]rune{'|', '/', '-', '\\'}
+	i := 0
+	for {
+		fmt.Printf("\r%c", chs[i])
+		i = (i + 1) % 4
+		time.Sleep(100 * time.Millisecond)
+	}
 }
